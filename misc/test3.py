e800cca4b8a3df52c58d506097d0d42b996df7ac
RiyyMiruku <no.why0919@gmail.com>
2025-05-25 23:22:12 +0800

    修正休市邏輯

--- misc/test3.py
+++ misc/test3.py
@@ -1,25 +1,23 @@
 import yfinance as yf
-import datetime
+from datetime import datetime, timedelta
 
 def is_today_market_closed(symbol: str) -> bool:
   
-    today = datetime.datetime.now().date()
-    data = yf.download(symbol, start=today, end=today + datetime.timedelta(days=1), progress=False)
+    today = datetime.now().date()
+    data = yf.download(symbol, start=today, end=today + timedelta(days=1), progress=False)
 
-    if data.empty:
-        return False
-    else:
-        return True
-    
+    #因為yfinance如果在非交易日下載會回傳前一天的資料，所以需要檢查第一筆資料的日期是否為今天
+
+    #若下載的筆數非空，判斷下載的資料的日期是否為今天
+    if not data.index.empty:
+        # 若第一筆資料的日期為今天，表示今天是交易日
+        if data.index[0].date() == today:
+            return True
+    return False
 
 if __name__ == '__main__':  
     symbol = "^TWII"
     # closed = is_today_market_closed(symbol)
     # print(f"Is the market closed for {symbol} today? {closed}")
 
-    today = datetime.datetime.now().date()
-    df = yf.download(symbol, start=today+datetime.timedelta(days=-1), end=today , progress=False)
-
-    print(df.index[0].date() != today+datetime.timedelta(days=-2))
-    print(today+datetime.timedelta(days=-2))
-    print(df.index[0].date())
+    print(is_today_market_closed(symbol))
--- news_to_index.py
+++ news_to_index.py
@@ -17,11 +17,13 @@ def is_today_market_closed(symbol: str) -> bool:
     data = yf.download(symbol, start=today, end=today + timedelta(days=1), progress=False)
 
     #因為yfinance如果在非交易日下載會回傳前一天的資料，所以需要檢查第一筆資料的日期是否為今天
-    if data.index[0].date() == today:
-        return True
-    #若下載資料不等於今天，表示今天其實是休市
-    else:
-        return False
+
+    #若下載的筆數非空，判斷下載的資料的日期是否為今天
+    if not data.index.empty:
+        # 若第一筆資料的日期為今天，表示今天是交易日
+        if data.index[0].date() == today:
+            return True
+    return False
 
 if __name__ == '__main__':
      # 抓取當日新聞
