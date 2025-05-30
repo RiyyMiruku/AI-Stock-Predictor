95a175f28ad37c4043b59f49442422d6e4ef67ba
RiyyMiruku <no.why0919@gmail.com>
2025-05-30 21:29:09 +0800

    更新資料

--- daily_tech_factor.py
+++ daily_tech_factor.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def calc_rsi(series, period=14):
     delta = series.diff()
-    gain = (delta.where(delta > 0, 0)).rolling(period).mean()
+    gain = (delta.where(delta        > 0, 0)).rolling(period).mean()
     loss = (-delta.where(delta < 0, 0)).rolling(period).mean()
     rs = gain / loss
     return 100 - (100 / (1 + rs))
